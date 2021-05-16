bc82ae37ab61a3b7770cd14f21d6e2b3fe7aa9e1
anthowen <anthowen@users.noreply.github.com>
2021-05-15 21:45:49 -0400

    WIP frontend integration

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,7 @@
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const { ethers } = require("hardhat");
+const { deploy } = require("./utils");
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,29 +15,36 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const ZombieFactory = await ethers.getContractFactory("ZombieFactory");
-  const ZombieFeeding = await ethers.getContractFactory("ZombieFeeding");
-  const ZombieHelper = await ethers.getContractFactory("ZombieHelper");
-  const ZombieAttack = await ethers.getContractFactory("ZombieAttack");
-  const ZombieOwnership = await ethers.getContractFactory("ZombieOwnership");
+  // const ZombieFactory = await ethers.getContractFactory("ZombieFactory");
+  // const ZombieFeeding = await ethers.getContractFactory("ZombieFeeding");
+  // const ZombieHelper = await ethers.getContractFactory("ZombieHelper");
+  // const ZombieAttack = await ethers.getContractFactory("ZombieAttack");
+  // const ZombieOwnership = await ethers.getContractFactory("ZombieOwnership");
 
-  const factory = await ZombieFactory.deploy();
-  const feeding = await ZombieFeeding.deploy();
-  const helper = await ZombieHelper.deploy();
-  const attack = await ZombieAttack.deploy();
-  const ownership = await ZombieOwnership.deploy();
+  // const factory = await ZombieFactory.deploy();
+  // const feeding = await ZombieFeeding.deploy();
+  // const helper = await ZombieHelper.deploy();
+  // const attack = await ZombieAttack.deploy();
+  // const ownership = await ZombieOwnership.deploy();
 
-  await factory.deployed();
-  await feeding.deployed();
-  await helper.deployed();
-  await attack.deployed();
-  await ownership.deployed();
+  // await factory.deployed();
+  // await feeding.deployed();
+  // await helper.deployed();
+  // await attack.deployed();
+  // await ownership.deployed();
 
-  console.log("factory address", factory.address);
-  console.log("feeding address", feeding.address);
-  console.log("helper address", helper.address);
-  console.log("attack address", attack.address);
-  console.log("ownership address", ownership.address);
+  // console.log("factory address", factory.address);
+  // console.log("feeding address", feeding.address);
+  // console.log("helper address", helper.address);
+  // console.log("attack address", attack.address);
+  // console.log("ownership address", ownership.address);
+  
+
+  await deploy("ZombieFactory");
+  await deploy("ZombieFeeding");
+  await deploy("ZombieHelper");
+  await deploy("ZombieAttack");
+  await deploy("ZombieOwnership");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
